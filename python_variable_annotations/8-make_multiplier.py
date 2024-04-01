fc5b97ca3187cdeda49a48056ce3353f07ffc1b4
RuslanHamidov <rhamidov202@gmail.com>
2024-04-01 15:54:43 +0400

    commit

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Description: function make_multiplier that takes
+a float multiplier as argument and returns a
+function that multiplies a float by multiplier.
+Arguments: multiplier: float
+'''
+
+
+def make_multiplier(multiplier: float) -> function:
+    '''returns function'''
+    return  multiplier * multiplier
