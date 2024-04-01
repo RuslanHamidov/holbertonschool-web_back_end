4b259b40b7836ba266274e6bc912e27441e0b2e2
RuslanHamidov <rhamidov202@gmail.com>
2024-04-01 23:22:55 +0400

    conc

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-''' Description: asynchronous coroutine that takes in an integer argument
-                 (max_delay, with a default value of 10) named wait_random
-                 that waits for a random delay between 0 and max_delay
-                 (included and float value) seconds and eventually returns it
+''' Description: async routine called wait_n that takes
+    in 2 int arguments (in this order): n and max_delay.
+    You will spawn wait_random n times with the specified max_delay.
     Arguments: max_delay: int = 10
 '''
 
 from typing import List
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
