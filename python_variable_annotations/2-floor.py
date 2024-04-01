d0ca4ab6606a8a3c567902cc2edf40be86ea67b5
RuslanHamidov <rhamidov202@gmail.com>
2024-04-01 15:03:53 +0400

    commit

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-Description: function floor which 
+Description: function floor which
 takes a float n as argument and returns the floor of the float.
 Arguments: n: float
 '''
