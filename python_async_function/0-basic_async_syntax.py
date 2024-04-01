8e38affa523db7551d9673fa934e208c7fea5e99
RuslanHamidov <rhamidov202@gmail.com>
2024-04-01 20:56:05 +0400

    async

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,9 +6,10 @@ named wait_random that waits for a random delay between 0 and max_delay
 '''
 
 import random, asyncio
+from typing import Union
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> Union[int,float]:
     '''return max_delay after await'''
     await asyncio.sleep(random.randint(0, max_delay))
     return max_delay
