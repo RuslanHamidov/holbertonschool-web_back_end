16668f67a6de87fa90341cee0f9878835d9b76e3
RuslanHamidov <rhamidov202@gmail.com>
2024-04-01 14:55:26 +0400

    commit

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 ''' Description: type-annotated function add that takes a float a and a float b
-                 as arguments and returns their sum as a float
+    as arguments and returns their sum as a float
     Arguments: a: float
-                b: float
+    b: float
 '''
 
 
