311e42cc0d91ba78ce37232d805b2e4c71553210
RuslanHamidov <rhamidov202@gmail.com>
2024-04-01 14:56:55 +0400

    commit

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-''' Description: function concat that takes a string str1 and a string str2 as arguments 
+''' 
+    Description: function concat that takes a string str1 and a string str2 as arguments 
     and returns a concatenated string
     Arguments: a: string
     b: string
