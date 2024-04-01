942134f222a7ca95ff39c74489db2d5012dfef9c
RuslanHamidov <rhamidov202@gmail.com>
2024-04-02 00:40:39 +0400

    conc

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-''' Description: async routine called wait_n that takes
-    in 2 int arguments (in this order): n and max_delay.
-    You will spawn wait_random n times with the specified max_delay.
-    Arguments: max_delay: int = 10
+''' Description: Import wait_random from the previous python file that
+                 you’ve written and write an async routine called wait_n
+                 that takes in 2 int arguments: max_delay and n. You will
+                 spawn wait_random n times with the specified max_delay.
+
+                 wait_n should return the list of all the delays(float values)
+                 The list of the delays should be in ascending order without
+                 using sort() because of concurrency.
+    Arguments: n: int, max_delay: int = 10
 '''
 
 from typing import List
@@ -15,7 +20,6 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     for i in range(n):
         await_list.append(await wait_random(max_delay))
     return sort_list(await_list)
-
 
 def sort_list(await_list: List[float]) -> List[float]:
     '''sorts list of awaits'''
