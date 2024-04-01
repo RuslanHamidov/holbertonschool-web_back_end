8e1da67ff2ea36004eebb0eb1c128542aed24261
RuslanHamidov <rhamidov202@gmail.com>
2024-04-01 14:00:55 +0400

    0-add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python3
+
+'''
+python3 -c 'print(__import__("my_module").__doc__)'
+'''
+
+
 def add(a:float, b:float) -> float:
     sum: float = a + b
     return sum
-
-print(add(5, 2))
