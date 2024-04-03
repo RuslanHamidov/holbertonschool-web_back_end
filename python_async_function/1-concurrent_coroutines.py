ded34088e03f87cf1a9ccfd8685c68d395cdbbb4
RuslanHamidov <rhamidov202@gmail.com>
2024-04-03 16:11:34 +0400

    conc

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,37 +1,25 @@
 #!/usr/bin/env python3
-'''
-Description: Import wait_random from the previous python file that
-you’ve written and write an async routine called wait_n
-that takes in 2 int arguments: max_delay and n. You will
-spawn wait_random n times with the specified max_delay.
-wait_n should return the list of all the delays(float values)
-The list of the delays should be in ascending order without
-using sort() because of concurrency.
-Arguments: n: int, max_delay: int = 10
-'''
-
+""" continue at the same time witha sync """
+import asyncio
+import random
 from typing import List
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''returns list of awaits'''
-    await_list: List[float] = []
-    for i in range(n):
-        await_list.append(await wait_random(max_delay))
-    return sort_list(await_list)
+async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
+    """
+    float time random
+    """
+    delays: List[float] = []
+    tasks: List = []
 
-def sort_list(await_list: List[float]) -> List[float]:
-    '''sorts list of awaits'''
-    n = len(await_list)
-    for i in range(n - 1):
-        flag = 0
-        for j in range(n - 1):
-            if await_list[j] > await_list[j + 1]:
-                tmp = await_list[j]
-                await_list[j] = await_list[j + 1]
-                await_list[j + 1] = tmp
-                flag = 1
-        if flag == 0:
-            break
-    return await_list
+    for _ in range(n):
+        tasks.append(wait_random(max_delay))
+
+    for task in asyncio.as_completed((tasks)):
+        delay = await task
+        delays.append(delay)
+
+    return delays
