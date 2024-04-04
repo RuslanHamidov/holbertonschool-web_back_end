9ea668626e1823306f92925cf7097f324b6dd863
RuslanHamidov <rhamidov202@gmail.com>
2024-04-04 19:52:40 +0400

    runtime

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -12,9 +12,11 @@ from typing import List, Generator
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
-    '''async function that returns runtime of 4 async_comp'''
-    start_time = asyncio.get_event_loop().time()
-    await asyncio.gather(async_comprehension(), async_comprehension(),async_comprehension(), async_comprehension())
-    end_time = asyncio.get_event_loop().time()
-    return end_time - start_time
+async def measure_runtime() -> float:
+    """measure_runtime should measure the
+    total runtime and return it"""
+    start = time.time()
+    await asyncio.gather(async_comprehension())
+    end = time.time()
+    total = end - start
+    return total
