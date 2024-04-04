23726555476dffdc5ac9c634496bfbd39a39d0ac
RuslanHamidov <rhamidov202@gmail.com>
2024-04-04 17:13:06 +0400

    tasks

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" function measures wait_n execution time """
+""" returns create_task """
 import asyncio
-import time
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''returns create task'''
     task1 = asyncio.create_task(wait_random(max_delay))
     return task1
