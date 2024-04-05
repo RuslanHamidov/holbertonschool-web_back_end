6654af11bfa096e41862a7515bcc83c1004fa9c7
RuslanHamidov <rhamidov202@gmail.com>
2024-04-05 21:48:55 +0400

    LIFO

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''
+class FIFOCache that inherits from BaseCaching and is a caching system
+'''
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        '''puts item to key element'''
+        if key and item:
+            if self.cache_data.__len__ > BaseCaching.MAX_ITEMS:
+                last = self.cache_data.popitem()
+                print("DISCARD:", last)
+
+    def get(self, key):
+        '''get key from cache_data'''
+        if key in self.cache_data:
+            return self.cache_data[key]
