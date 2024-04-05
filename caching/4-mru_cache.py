b434476f05b851e9d74cecbb63bd562c6d2ff261
RuslanHamidov <rhamidov202@gmail.com>
2024-04-05 22:07:06 +0400

    MRU

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -12,11 +12,10 @@ class MRUCache(BaseCaching):
     def put(self, key, item):
         '''puts item to key element'''
         if key and item:
+            if self.cache_data.__len__() + 1 > BaseCaching.MAX_ITEMS:
+                last = self.cache_data.popitem()
+                print("DISCARD:", last[0])
             self.cache_data[key] = item
-            if self.cache_data.__len__() > BaseCaching.MAX_ITEMS:
-                first = next(iter(self.cache_data))
-                print("DISCARD:", first)
-                self.cache_data.pop(first)
 
     def get(self, key):
         ''' Get an item by key
@@ -24,4 +23,5 @@ class MRUCache(BaseCaching):
         if key in self.cache_data:
             value = self.cache_data[key]
             self.cache_data.pop(key)
+            self.cache_data[key] = value
             return value
