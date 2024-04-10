036306dfe1cfac8689e2930d8e40efaacef1a88a
RuslanHamidov <rhamidov202@gmail.com>
2024-04-10 21:02:44 +0400

    simple

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
-'''
-The function should return a tuple of size two
-containing a start index and an end index corresponding
-to the range of indexes to return in a
-list for those particular pagination parameters.
+'''Description: Implement a method named get_page that takes two integer
+                arguments page with default value 1 and page_size with
+                default value 10.
+
+    - You have to use this CSV file (same as the one presented at the top
+      of the project)
+    - Use assert to verify that both arguments are integers greater than 0.
+    - Use index_range to find the correct indexes to paginate the dataset
+      correctly and return the appropriate page of the dataset (i.e. the
+    correct list of rows).
+    - If the input arguments are out of range for the dataset, an empty list
+      should be returned.
 '''
 
 import csv
@@ -19,6 +26,7 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        ''' Initialize instance. '''
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -33,14 +41,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        '''get page method'''
+        ''' Return page of dataset. '''
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
-        
+
         indices = index_range(page, page_size)
         start = indices[0]
         end = indices[1]
-        
+
         try:
             return self.dataset()[start:end]
         except IndexError:
