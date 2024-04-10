18ce9f66dd1989f26320bf8a5d918aebedaa4728
RuslanHamidov <rhamidov202@gmail.com>
2024-04-10 20:50:28 +0400

    range

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -9,6 +9,6 @@ list for those particular pagination parameters.
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple(int, int):
+def index_range(page: int, page_size: int) -> Tuple[int]:
     '''function for pagination'''
     return ((page - 1) * page_size, page * page_size)
