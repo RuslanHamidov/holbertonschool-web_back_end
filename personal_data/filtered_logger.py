c8bcd13db54aa0c7532a97b10aeaa5cc6fa612c2
RuslanHamidov <rhamidov202@gmail.com>
2024-04-17 22:18:14 +0400

    personal data initial commit

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Function called filter_datum that returns the log message obfuscated
+"""
+
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+    '''function to make log message obfuscated'''
+    pass
