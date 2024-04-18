7c6c9fccd2c234bb6124ba4a98f3659d6ddb1f89
RuslanHamidov <rhamidov202@gmail.com>
2024-04-18 17:27:54 +0400

    filtered1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -24,6 +24,7 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        ''' method initializer '''
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
