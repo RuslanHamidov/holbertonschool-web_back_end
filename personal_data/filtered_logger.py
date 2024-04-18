e153a6224d1db593a803cb1ee9decd2ff2b9db90
RuslanHamidov <rhamidov202@gmail.com>
2024-04-18 17:23:57 +0400

    filtered1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -10,8 +10,7 @@ import os
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                    separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     '''Filtering values of the required fields'''
     for item in fields:
         message = re.sub(f"{item}=.*?{separator}",
