40c2bf7e97e47f982572a3e6b207a2afbac45115
RuslanHamidov <rhamidov202@gmail.com>
2024-04-25 20:20:39 +0400

    add create user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -17,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -32,9 +32,12 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        '''add user to database
-        '''
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        """Save the user to the database
+        """
+        user = User()
+        user.email = email
+        user.hashed_password = hashed_password
+        session = self._session
+        session.add(user)
+        session.commit()
         return user
