ac433500830ec24d7a2d416a0a913a0dc0319251
RuslanHamidov <rhamidov202@gmail.com>
2024-04-25 19:53:59 +0400

    add create user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -32,10 +32,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         '''add user to database
         '''
-        user = User()
-        user.email = email
-        user.hashed_password = hashed_password
-        session = self._session
-        session.add(user)
-        session.commit()
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
         return user
