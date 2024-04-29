50cf95927764bc387b7cc2a7d426e4a5da27b1ae
RuslanHamidov <rhamidov202@gmail.com>
2024-04-29 20:28:47 +0400

    second

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -11,12 +11,13 @@ from functools import wraps
 class TestAccessNestedMap(unittest.TestCase):
     '''Testing access method to check path of nested map
     '''
+    
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(self, nested_map, path_map, result_expec):
         ''' testing access to map
         '''
-        self.assertEqual(access_nested_map((nested_map, path), expected))
+        self.assertEqual(access_nested_map(nested_map, path_map), result_expec)
