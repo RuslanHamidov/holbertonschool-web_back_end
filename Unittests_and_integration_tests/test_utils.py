4159235f7143a03d8134024a86fa0c4de0dcd1b3
RuslanHamidov <rhamidov202@gmail.com>
2024-04-30 18:57:05 +0400

    add test_memoize

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -51,3 +51,25 @@ class TestGetJson(unittest.TestCase):
         with patch('requests.get') as mock_request:
             mock_request.return_value.json.return_value = test_payload
             self.assertEqual(get_json(url=test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    ''' Class to test memoize method
+    '''
+    def test_memoize(self):
+        ''' test_memoize method
+        '''
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+    
+        with patch.object(TestClass, 'a_method') as mock_a_method:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock_a_method.assert_called_once()
