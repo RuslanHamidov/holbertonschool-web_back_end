7197d1ec48110c7bbbd335c87543dff96c1266e1
RuslanHamidov <rhamidov202@gmail.com>
2024-04-30 19:12:33 +0400

    add test_client

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Test Client
+"""
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from parameterized import parameterized, parameterized_class
+import json
+import unittest
+from unittest.mock import patch, PropertyMock, Mock
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    ''' Testing GithubOrgClient class
+    '''
+    @parameterized.expand(
+        ('google'),
+        ('abc')
+    )
+    @patch('client.get_json')
+    def test_org(self, input, mock):
+        ''' testing org method
+        '''
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
