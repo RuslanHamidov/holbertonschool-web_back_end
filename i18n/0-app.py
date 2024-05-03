b1075b00624f0a6f5bcb9a9c7916acf59dad015f
RuslanHamidov <rhamidov202@gmail.com>
2024-05-03 20:22:27 +0400

    initial

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-''' module to learn i18n
-'''
-from flask import Flask, request, render_template
+""" Basic Flask app """
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    ''' returns index.html file
-    '''
-    return render_template('index.html')
+    """ Greeting
+
+        Return:
+            Initial template html
+    """
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
