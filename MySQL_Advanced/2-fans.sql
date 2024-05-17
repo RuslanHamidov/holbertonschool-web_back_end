cc7346ae7ebc074ec1c7679e61c6665ac5348e56
RuslanHamidov <rhamidov202@gmail.com>
2024-05-17 21:27:08 +0400

    add 2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,11 +1,5 @@
 -- SQL script that ranks country origins of bands
 -- Creates table band_fans
-CREATE TABLE IF NOT EXISTS band_fans (
-    origin VARCHAR(255),
-    nb_fans INT
-);
--- Inserts into fans
-INSERT INTO band_fans (origin, nb_fans)
-SELECT origin, fans
-FROM metal_bands
+SELECT origin AS origin, fans AS nb_fans
+FROM metal_bands 
 ORDER BY fans DESC;
