3a0dfbddb46b986fe8ec1043059680ebfc400c85
RuslanHamidov <rhamidov202@gmail.com>
2024-05-17 20:33:23 +0400

    update 0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- SQL script that creates a table users
 -- Table creation script
-CREATE TABLE table (id INT NOT NULL AUTOINCREMENT PRIMARY KEY, 
+CREATE TABLE users (id INT NOT NULL AUTOINCREMENT PRIMARY KEY, 
                     email VARCHAR(255) NOT NULL UNIQUE,
                     name VARCHAR(255)
                     );
