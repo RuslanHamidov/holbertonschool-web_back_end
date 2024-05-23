614f855ad6cc8020c77bfd280ef3b7a0dcbc9d6a
RuslanHamidov <rhamidov202@gmail.com>
2024-05-23 21:43:43 +0400

    4-rest

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
 export default function returnHowManyArguments(...theArgs) {
-  let numOfArgs = 0;
-  for (const arg of theArgs) {
-    numOfArgs += 1;
-  }
-  return numOfArgs;
+  return theArgs.length;
 }
