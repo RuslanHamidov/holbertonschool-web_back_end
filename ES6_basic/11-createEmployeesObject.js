b09dac18dd4626fdc71864cd90535f2a10a4392b
RuslanHamidov <rhamidov202@gmail.com>
2024-05-24 17:26:02 +0400

    final 11

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,3 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const employee = {
-    [`${departmentName}`]: [`${employees}`],
-  };
-  return employee;
+  return { [departmentName]: employees };
 }
