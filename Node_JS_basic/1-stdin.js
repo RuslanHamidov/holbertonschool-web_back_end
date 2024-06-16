f9f2ef45f365cf2b1883b2b12fb5c292aafc933e
RuslanHamidov <rhamidov202@gmail.com>
2024-06-16 14:48:06 +0400

    final 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,13 @@
-var standard_input = process.stdin;
-standard_input.setEncoding('utf-8');
-console.log("Welcome to Holberton School, what is your name?");
-standard_input.on('data', function(data) {
-    console.log('Your name is: ' + data);
-    console.log("This important software is now closing");
-    process.exit();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+    const chunk = process.stdin.read();
+    if (chunk !== null) {
+        process.stdout.write(`Your name is ${chunk}`)
+    }
 });
+
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
