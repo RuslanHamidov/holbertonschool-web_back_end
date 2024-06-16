a795aaf7c3779661c18352bacc9be8d4365b0d5f
RuslanHamidov <rhamidov202@gmail.com>
2024-06-16 14:32:39 +0400

    add files and 1-stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,3 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (input) => {
-    console.log(`Your name is: ${input}`);
-    rl.close();
-});
+var readline = require('readline-sync');
+var name = readline.question("What is your name?\n");
+console.log("Hi " + name + ", nice to meet you.");
